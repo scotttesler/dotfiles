d7b4226c4a606a7f4647a75ccedd3910076d6604
scotttesler <scottiet123@gmail.com>
2019-10-07 22:28:16 -0500

    Make zsh plugins not fail when some functions don't exist

---
+++ zsh/custom/plugins/git-scotttesler/git-scotttesler.plugin.zsh
@@ -0,0 +1,66 @@
+function db() {
+  local current_branch=`eval "git rev-parse --abbrev-ref HEAD"`
+
+  if [[ "$current_branch" == "master" ]]; then
+    echo "Current branch is master. Cannot delete master."
+    return 0
+  fi
+
+  echo "Are you sure you want to delete branch $current_branch?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes ) break;;
+      No ) return 0;;
+    esac
+  done
+
+  git checkout master
+  git branch -D $current_branch
+  gf
+}
+
+function gd() {
+  git diff
+}
+
+function gf() {
+  git fetch --all --prune
+  git pull
+}
+
+function gl() {
+  git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+}
+
+function gpfwl() {
+  git push --force-with-lease
+
+  if ( user_has cowsay ); then
+    if ( user_has lolcatjs ); then
+      cowsay -r 'Force push (with lease)' | lolcatjs
+    else
+      cowsay -r 'Force push (with lease)'
+    fi
+  fi
+}
+
+function gri() {
+  git rebase -i origin/master
+}
+
+function gs() {
+  git status
+}
+
+function gwip() {
+  git add .
+  git commit -m 'WIP'
+  git push
+}
+
+function qf() {
+  git add .
+  git commit -m '.'
+  gri
+  gpfwl
+}
