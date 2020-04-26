3c20a9d1a8f31b14051ddda3439dc73084d67486
scotttesler <scottiet123@gmail.com>
2020-04-25 23:23:21 -0500

    Use my fork of lightline
    With the "one" theme, the entire line shows color in insert mode

--- install.zsh
+++ install.zsh
@@ -88,7 +88,7 @@ function install_vim_start_packages() {
   cd "$VIM_DIR/pack/packages/start"
 
   git clone https://github.com/ryanoasis/vim-devicons.git dev-icons
-  git clone https://github.com/itchyny/lightline.vim lightline
+  git clone https://github.com/scotttesler/lightline.vim lightline
   git clone https://github.com/scrooloose/nerdtree.git
   git clone https://github.com/tomtom/tcomment_vim.git tcomment
   git clone https://github.com/pangloss/vim-javascript.git javascript
